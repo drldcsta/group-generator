f065737b53bb29954734216151a863f7bcbc5ea9
Darrell DeCosta <dar.deco+git@gmail.com>
2018-06-21 00:28:22 -0400

    refactored key up event handler to grow and shrink, fixed 'enter' key bug

--- main.js
+++ main.js
@@ -5,33 +5,38 @@ const textarea = document.querySelector('textarea');
 const button = document.querySelector('button')
 
 
-textarea.addEventListener('keyup', autosize);
+textarea.addEventListener('keyup', function () {
+    const el = this;
+    let textContent = el.value;
+    let contentLength = textContent.split("\n").length
+    let rows = Number(el.getAttribute("rows"))
+    // console.log(textContent)
+    if (textContent.indexOf(",") > 0 || contentLength > rows) {
+        const newContent = textContent.replace(",", "\n");
+        el.value = newContent;
+        console.log("dogs");
+        el.setAttribute("rows", ++rows)
+    } else {
+        el.setAttribute("rows", contentLength)
+
+    }
+});
+
+
+
+
 button.addEventListener('click', function handleSubmit(e) {
     e.preventDefault();
     let names = textarea.value
     let nameList = names.split("\n")
     let groupSize = Number(document.querySelector("select#team-size").value)
     // console.table(nameList)
-    console.log(generateGroups(nameList,groupSize))
+    console.log(generateGroups(nameList, groupSize))
 });
 
 
 
-//inspired by https://codepen.io/vsync/pen/czgrf (but mine is better)
-function autosize() {
-    const el = this;
-    let textContent = el.value;
-    if (textContent.indexOf(",") > 0) {
-        const newContent = textContent.replace(",", "\n");
-        el.value = newContent;
-        console.log(newContent);
-        setTimeout(function () {
-            let rows = Number(el.getAttribute("rows"))
-            el.setAttribute("rows", ++rows)
 
-        }, 0);
-    }
-}
 
 function generateGroups(arr, n) {
     let groupsArray = [];
