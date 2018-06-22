597f7406dce5a0e8e4777827e87f763f97f108bf
Darrell DeCosta <dar.deco+git@gmail.com>
2018-06-21 21:03:47 -0400

    styling tweaks and bug fixes

--- main.js
+++ main.js
@@ -3,18 +3,22 @@ console.log("looks like we made it")
 
 const textarea = document.querySelector('textarea');
 const button = document.querySelector('button')
+const teams = document.querySelector('.teams-wrapper')
+
 
 
 //TODO - find a way to set a limit so text area can't overrun it's container
 textarea.addEventListener('keyup', function () {
     const el = this;
-    const textContent = el.value;
+    let textContent = el.value;
+    console.log(textContent.split("\n"))
     let contentLength = textContent.split("\n").length
     let rows = Number(el.getAttribute("rows"))
     if (textContent.indexOf(",") > 0 || contentLength > rows) {
         const newContent = textContent.replace(/,/g, "\n");
         el.value = newContent;
-        el.setAttribute("rows", contentLength)
+        let newLength = newContent.split("\n").length
+        el.setAttribute("rows", newLength)
     } else {
         el.setAttribute("rows", contentLength)
 
@@ -22,13 +26,16 @@ textarea.addEventListener('keyup', function () {
 });
 
 
+
 button.addEventListener('click', function handleSubmit(e) {
+    clearTeams();
     e.preventDefault();
     let names = textarea.value
     let nameList = names.split("\n")
     let groupSize = Number(document.querySelector("select#team-size").value)
     // console.table(nameList)
-    console.log(generateGroups(nameList, groupSize))
+    // console.log(generateGroups(nameList, groupSize))
+    generateGroups(nameList, groupSize)
 });
 
 
@@ -37,7 +44,13 @@ button.addEventListener('click', function handleSubmit(e) {
 
 function generateGroups(arr, n) {
     let groupsArray = [];
-
+    arr = arr.filter(name => {
+        if (name.length > 0) {
+            return true
+        } else {
+            return false
+        }
+    })
     function shuffleArray(sourceArray) {
         for (let i = 0; i < sourceArray.length - 1; i++) {
             let randIndex = i + Math.floor(Math.random() * (sourceArray.length - i));
@@ -52,12 +65,15 @@ function generateGroups(arr, n) {
         let xxx = shufStuds.splice(0, n);
         groupsArray.push(xxx);
     }
-    console.log(groupsArray)
-     buildDom(groupsArray);
+    // console.log(groupsArray)
+    buildDom(groupsArray);
+}
+
+function clearTeams () {
+    teams.innerHTML = ""
 }
 
 function buildDom(groupsArray) {
-    const teams = document.querySelector('.teams-wrapper')
     groupsArray.forEach(group => {
         let teamBox = document.createElement('div');
         teamBox.setAttribute('class', 'team-box')
@@ -65,7 +81,7 @@ function buildDom(groupsArray) {
             let memberWrapper = document.createElement('div')
             memberWrapper.setAttribute('class', 'member-wrapper')
             let teamMember = document.createElement('div')
-            teamMember.setAttribute('class','team-member')
+            teamMember.setAttribute('class', 'team-member')
             let memberName = document.createTextNode(person)
             teamMember.appendChild(memberName)
             memberWrapper.appendChild(teamMember)
